25d63a3efef89f502b7b1c54f2607f5f13a9d615
Ilia Gogotchuri <igogo17@freeuni.edu.ge>
2020-02-11 11:10:07 +0400

    updated script, to cut test data
    Now takes argument 1, folder which contains test audio in wav format, resamples, reduces to mono channel and then cuts loudest section of the voice (word itself).

--- cut_voices.sh
+++ cut_voices.sh
@@ -1,13 +1,26 @@
 #!/usr/bin/env bash
 
 currDir=$(cd `dirname $0` && pwd)
+voiceDir=${1}
 execFile=${currDir}/vendor/extract_loudest_part
-saveDir=${currDir}/data_cut
+tmpDir=${currDir}/tmp_voices/
+saveDir=${currDir}/test_cut/
 
-echo "Cleaning old cuts..."
+echo "Removing old test files..."
 rm -rf ${saveDir}
-for filename in ${currDir}/data_raw/*.wav; do
+echo "Converting all audio to mono channel, 16Khz..."
+mkdir -p ${tmpDir}
+mkdir -p ${saveDir}
+for filename in ${voiceDir}/*.wav; do
+    baseFilename=$(basename ${filename})
+    ffmpeg -loglevel panic -i "${filename}" -acodec pcm_s16le -ac 1 -ar 16000 "${tmpDir}${baseFilename}"
+done
+echo "Starting to seach for loudest sections in each file...."
+for filename in ${tmpDir}*.wav; do
     echo "Cutting file: ${filename}"
     ${execFile} ${filename} ${saveDir}
 done
+echo "Cleaning up intermediates..."
+rm -rf ${tmpDir}
+echo "Files are written to ${saveDir}"
 echo "All done!"
